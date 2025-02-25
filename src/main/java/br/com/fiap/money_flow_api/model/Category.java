21ccf01e6ee42413b602933a12c55acc7b5a89fb
NicolasDobbeck <nicolasdobbeckmendes12@gmail.com>
2025-02-25 20:53:43 -0300

    Endpoints: Post e GetById

--- src/main/java/br/com/fiap/money_flow_api/model/Category.java
+++ src/main/java/br/com/fiap/money_flow_api/model/Category.java
@@ -1,12 +1,14 @@
 package br.com.fiap.money_flow_api.model;
 
+import java.util.Random;
+
 public class Category {
     private Long id;
     private String nome;
     private String icon;
     
     public Category(Long id, String nome, String icon) {
-        this.id = id;
+        this.id = (id == null) ? new Random().nextLong() : id;
         this.nome = nome;
         this.icon = icon;
     }
