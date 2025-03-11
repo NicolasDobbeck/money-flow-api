e398ae213b426977705a6e6e845779b46e6a401f
NicolasDobbeck <nicolasdobbeckmendes12@gmail.com>
2025-03-10 21:31:39 -0300

    Fazendo o delete do crud de categorias

--- src/main/java/br/com/fiap/money_flow_api/controller/CategoryController.java
+++ src/main/java/br/com/fiap/money_flow_api/controller/CategoryController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,4 +60,20 @@ public class CategoryController {
     }
 
     //Apagar categoria
+
+    @DeleteMapping("/categories/{id}")
+    public ResponseEntity<Object> detroy(@PathVariable Long id){
+        System.out.println("Apagando categoria" + id);
+
+        var category = repository
+            .stream()
+            .filter(c -> c.getId().equals(id))
+            .findFirst();
+
+        if (category.isEmpty()) {
+           return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.noContent().build();
+    }
 }
