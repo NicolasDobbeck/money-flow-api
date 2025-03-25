aca29846b58cee7e9a7a078fc981ffd043d73ea1
NicolasDobbeck <nicolasdobbeckmendes12@gmail.com>
2025-03-24 22:45:56 -0300

    Utilizando o categoryRepository

--- src/main/java/br/com/fiap/money_flow_api/controller/CategoryController.java
+++ src/main/java/br/com/fiap/money_flow_api/controller/CategoryController.java
@@ -1,12 +1,10 @@
 package br.com.fiap.money_flow_api.controller;
 
-
-
-import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,40 +13,43 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import br.com.fiap.money_flow_api.model.Category;
+import br.com.fiap.money_flow_api.repository.CategoryRepository;
 
 
-
+@RequestMapping("/categories")
 @RestController //component
 public class CategoryController {
     
     private Logger log = LoggerFactory.getLogger(getClass());
-    private List<Category> repository = new ArrayList<>();
+    @Autowired
+    private CategoryRepository repository;
 
     //Listar todas as categorias
     //GET :8080/categories -> 200 ok -> json  
-    @GetMapping(path = "/categories")
+    @GetMapping
     public List<Category> index(){
-        return repository;
+        return repository.findAll();
     }
 
     //Cadastrar categories
     //POST :8080/categories
-    @PostMapping("/categories")
+    @PostMapping
     //@ResponseStatus(value = HttpStatus.CREATED)
     public ResponseEntity<Category> create(@RequestBody Category category){
         log.info("Cadastrando.." + category.getNome());
-        repository.add(category);
+        repository.save(category);
         return ResponseEntity.status(201).body(category);
     }
 
     //Detalhes da catgories
     //POST :8080/categories/id
-    @GetMapping("/categories/{id}")
+    @GetMapping("/{id}")
     public Category get(@PathVariable Long id){
         log.info("Buscando categoria by id " + id);
 
@@ -57,32 +58,30 @@ public class CategoryController {
 
     //Apagar categoria
     //DELETE :8080/categories/id
-    @DeleteMapping("/categories/{id}")
+    @DeleteMapping("/{id}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT) //204
     public void detroy(@PathVariable Long id){
         log.info("Apagando categoria" + id);
 
-        repository.remove(getCategory(id));
+        repository.delete(getCategory(id));
     }
 
     //Editar categoria
     //PUT :8080/categories/id + bbody
-    @PutMapping("/categories/{id}")
+    @PutMapping("/{id}")
     public Category update(@PathVariable Long id, @RequestBody Category category){
         log.info("Atualizando categoria" + id + category);
 
-        repository.remove(getCategory(id));
+        getCategory(id);
         category.setId(id);
-        repository.add(category);
+        repository.save(category);
 
         return category;
     }
 
     private Category getCategory(Long id) {
         return repository
-            .stream()
-            .filter(c -> c.getId().equals(id))
-            .findFirst()
+            .findById(id)
             .orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
             );
--- src/main/java/br/com/fiap/money_flow_api/model/Category.java
+++ src/main/java/br/com/fiap/money_flow_api/model/Category.java
@@ -1,44 +1,17 @@
 package br.com.fiap.money_flow_api.model;
 
-import java.util.Random;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
 
+
+@Entity
+@Data
 public class Category {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String icon;
-    
-    public Category(Long id, String nome, String icon) {
-        this.id = (id == null) ? Math.abs(new Random().nextLong()) : id;
-        this.nome = nome;
-        this.icon = icon;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-
-    @Override
-    public String toString() {
-        return "Category [id=" + id + ", nome=" + nome + ", icon=" + icon + "]";
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
 }
